520017ccbc9784d5a2471e6988f170c84576494d
rouzbeh majlessi <rmajlessi@gmail.com>
2018-03-29 22:21:19 -0700

    Add Product array in root component and the instance variable of products array, and the select product eventemitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter} from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = "Rouz"
+  products: Product[];
+
+  constructor() {
+    this.products = [
+
+      new Product(
+      'MYSHOES',
+      'Nike Air Max',
+      '/assets/images/products/nike-shoes.jpg',
+      ['Men', 'Shoes', 'Running Shoes'],
+      109.99),
+      
+      new Product(
+        'NIKEJACKET',
+        'Blue Jacket',
+        '/assets/images/products/nike-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        283.99),
+
+        new Product(
+          'NIKEHAT',
+          'Nike Swoosh Hat',
+          '/assets/images/products/nike-hat.jpg',
+          ['Unisex', 'Accessories', 'Hats'],
+          29.99)
+    ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked:', product);
+  }
+
 }
